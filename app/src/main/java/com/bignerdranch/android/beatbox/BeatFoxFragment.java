139136860f2c2123389baa2481c17ac7729caad4
Ben-DG <bendgrafstein@gmail.com>
2018-05-14 11:49:35 -0700

    Done challenge (not tested)

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatFoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatFoxFragment.java
@@ -10,42 +10,94 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import com.bignerdranch.android.beatbox.databinding.FragmentBeatBoxBinding;
 import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding;
 
-public class BeatFoxFragment extends Fragment {
+import java.util.List;
+
+public class BeatFoxFragment extends Fragment{
+
+    private BeatBox mBeatBox;
 
     public BeatFoxFragment() {
     }
 
-    public static BeatFoxFragment newIstance() {
+    public static BeatFoxFragment newInstance() {
         return new BeatFoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true); // Retains the fragment when activity is destroyed on config change
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        FragmentBeatBoxBinding binding = DataBindingUtil
+        final FragmentBeatBoxBinding binding = DataBindingUtil
                 .inflate(inflater, R.layout.fragment_beat_box, container, false);
 
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-        binding.recyclerView.setAdapter(new SoundAdapter());
+        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
+        binding.seekBarText.setText(getResources().getString(R.string.playback_speed, mBeatBox.getPlayRate()));
+        binding.seekBar.setProgress(50);
+        binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mBeatBox.setPlayRate(i);
+                binding.seekBarText.setText(getResources().getString(R.string.playback_speed, mBeatBox.getPlayRate()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
     }
 
     private class SoundHolder extends RecyclerView.ViewHolder {
         private ListItemSoundBinding mBinding;
 
+        // Construct and attach the view model
         private SoundHolder(ListItemSoundBinding binding) {
             super(binding.getRoot());
             mBinding = binding;
+            mBinding.setViewModel(new SoundViewModel(mBeatBox));
+        }
+
+        // Update the data that the view model is working with
+        public void bind(Sound sound) {
+            mBinding.getViewModel().setSound(sound);
+            mBinding.executePendingBindings(); // Not necessarily needed but RecyclerView updates at very high speed, so added to ensure timely layout updates
         }
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+
         @Override
         public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getActivity());
@@ -56,12 +108,13 @@ public class BeatFoxFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(SoundHolder holder, int position) {
-
+            Sound sound = mSounds.get(position);
+            holder.bind(sound);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return mSounds.size();
         }
     }
 }
